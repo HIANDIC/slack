048822aaa3bd517857c92e49fb106593a3e3d7f5
HIANDIC <46762226+HIANDIC@users.noreply.github.com>
2020-05-13 16:59:05 +0200

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -6,5 +6,11 @@ public class CreateChannel {
 
         System.out.println("User is able to create channels");
     }
-
+    String s= "a";
+    if(s.contains("a")){
+         System.out.println("contains");
+    }else{
+         System.out.println("not");
+    }
+    
 }
